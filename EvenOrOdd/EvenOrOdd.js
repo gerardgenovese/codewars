9d253cb0930d53837479c86c86e6ba7a9966f646
Gerard <gerardgenovese24@gmail.com>
2017-12-07 22:07:57 -0500

    Update EvenOrOdd.js

--- EvenOrOdd/EvenOrOdd.js
+++ EvenOrOdd/EvenOrOdd.js
@@ -1,8 +1,8 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-// function even_or_odd(number) {
-//     return number % 2 === 0 ? "Even" : "Odd";
-//   }
+function even_or_odd(number) {
+    return number % 2 === 0 ? "Even" : "Odd";
+  }
 
 //   function even_or_odd(number) {
 //     if (number % 2 === 0) {
@@ -13,19 +13,5 @@
 //     }
 //   }
 
-
-
-
-
-
-function change(x) {
-    
-let y = x.replace(/-*_*/g, ' ');
-let z = y.replace(/\w\S*/g, function(elem){
-	return elem.charAt(0).toUpperCase() + elem.substr(1).toLowerCase();
-});
-	return a = z.replace(/\s+/g, "");
-}
-change('i-dont-care');
 
  
