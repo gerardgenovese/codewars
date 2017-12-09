e923f9c568c58f02119d8675f1af7deeb484116c
Gerard Genovese <gerardgenovese24@gmail.com>
2017-12-08 20:32:24 -0500

    Update Whos Move.js

--- whosMove/Whos Move.js
+++ whosMove/Whos Move.js
@@ -1,4 +1,7 @@
-// Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. If a player loses a round, it's the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
+// Two players - "black" and "white" are playing a game. The game consists of several rounds. 
+//If a player wins in a round, he is to move again during the next round. 
+//If a player loses a round, it's the other player who moves on the next round. 
+//Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
 
 function whoseMove(lastPlayer, win) {
   
@@ -16,6 +19,9 @@ function whoseMove(lastPlayer, win) {
    }
    
  };
+
+
+//Answer's from other Coders 
 
 //==========================================================================================================================================
 function whoseMove(lastPlayer, win) {
