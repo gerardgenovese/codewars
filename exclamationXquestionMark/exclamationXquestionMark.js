85e1ab74a1589ecd44f4831811bdf34a935b9dc7
Gerard Genovese <gerardgenovese24@gmail.com>
2017-12-11 21:51:49 -0500

    Update exclamationXquestionMark.js

--- exclamationXquestionMark/exclamationXquestionMark.js
+++ exclamationXquestionMark/exclamationXquestionMark.js
@@ -16,38 +16,8 @@ function product(s){
 }
 
 
+//Code from other people
 
-
-
-
-
-
-
-function product(s){
-var countp = 0;
-var countq = 0;
-
-  for( var i = 0; i < s.length; i++)
-  if( s[i] === '!'){
-    countp++;
-  }
-  for( var j = 0; j < s.length; j++)
-  if(s[j] === '?') {
-    countq++;   
-  }
-  return countp * countq  
-}
-
-product('???!!!');
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-function product(s){
-  excs = s.split('!').length - 1
-  ques = s.split('?').length - 1
-  return excs * ques
-}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -55,27 +25,6 @@ function product(s){
   return (s.match(/!/g) == null || s.match(/\?/g) == null) ? 0 : s.match(/!/g).length * s.match(/\?/g).length;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-function product(s){
-  //coding and coding....
-  var ques = 0;
-  var excla = 0;
- 
-    s.split('').forEach(function(item){
-      if (item === "!"){
-        excla += 1;
-      }
-      if(item === "?"){
-        ques += 1;
-      }
-      if(item === ""){
-        return 0;
-      }
-    });
-  
-  return excla * ques;
-}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -83,29 +32,6 @@ function product(s) {
   return s.replace(/\?+/g, "").length * s.replace(/!+/g, "").length
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-function product(s){
-  var first = 0;
-  var second = 0;
-  for(const elem of s){
-    if(elem == '!'){
-      first++;
-    }
-    else {
-      second++;
-    }
-  }
-  return first * second;
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-let product = (s) => s.replace(/[^!]/g, '').length * s.replace(/[^?]/g, '').length;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
