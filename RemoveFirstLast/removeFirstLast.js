ac9febe5494b122f77ed2787d28eaf64cbc5a075
Gerard Genovese <gerardgenovese24@gmail.com>
2017-12-19 18:30:24 -0500

    Add files via upload

---
+++ RemoveFirstLast/removeFirstLast.js
@@ -0,0 +1,16 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, except in C, where, to keep the difficulty at the level of the kata, you are given two parameters, the first a buffer with length exactly the same as the second parameter, the original string. You don't have to worry with strings with less than two characters.
+
+
+function removeChar(str){
+    
+      for (let i = 0; i < str.length; i++) {
+        let str2 = str.slice(1, str.length - 1);
+        return str2;
+      }  
+        
+};
+
+
+// function removeChar(str) {
+//     return str.slice(1, -1);
+//   }
