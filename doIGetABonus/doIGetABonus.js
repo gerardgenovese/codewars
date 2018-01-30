7ff26911855a57a4fd7c323ecc28f2f666e58bf3
Gerard Genovese <gerardgenovese24@gmail.com>
2018-01-29 22:26:04 -0500

    Update doIGetABonus.js

--- doIGetABonus/doIGetABonus.js
+++ doIGetABonus/doIGetABonus.js
@@ -3,9 +3,11 @@ It's bonus time in the big city! The fatcats are rubbing their paws in anticipat
 
 Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
 
-If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must 
+receive only his stated salary.
 
-Return the total figure the individual will receive as a string prefixed with '£' (JS and Java) or '$' (C#, C++, Ruby, Clojure, Elixir, PHP and Python).
+Return the total figure the individual will receive as a string prefixed with '£' (JS and Java) or '$' 
+(C#, C++, Ruby, Clojure, Elixir, PHP and Python).
 */
 
 function bonusTime(salary, bonus) {
